dc944a37a6841a0816392bc0f58e8817110f2413
MikPoik <42643139+MikPoik@users.noreply.github.com>
2025-02-05 21:09:02 +0000

    change buckets

--- src/gcp_constants.py
+++ src/gcp_constants.py
@@ -3,10 +3,10 @@ gcp_hmac_secret = modal.Secret.from_name(
 "gcp-secret",
 required_keys=["GOOGLE_ACCESS_KEY_ID", "GOOGLE_ACCESS_KEY_SECRET"]
 )
-#GCP_PUBLIC_IMAGE_BUCKET = 'truluv-public'
-#GCP_CHAT_BUCKET = 'truluv-chats'
+GCP_PUBLIC_IMAGE_BUCKET = 'truluv-public'
+GCP_CHAT_BUCKET = 'truluv-chats'
 
-GCP_PUBLIC_IMAGE_BUCKET = 'coqui-samples'
-GCP_CHAT_BUCKET = 'modal-agent-chat-test'
+#GCP_PUBLIC_IMAGE_BUCKET = 'coqui-samples'
+#GCP_CHAT_BUCKET = 'modal-agent-chat-test'
 
 GCP_BUCKET_ENDPOINT_URL = "https://storage.googleapis.com"
